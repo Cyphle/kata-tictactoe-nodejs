83d480bff332767077ce45ea13876e6204ec45e3
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-26 21:08:52 +0200

    Player O wins on row 1

--- src/domain/TicTacToeGame.ts
+++ src/domain/TicTacToeGame.ts
@@ -33,14 +33,16 @@ export default class TicTacToeGame {
   }
 
   private hasPlayerWon(player: string): boolean {
-    let hasWonInLine = this.hasPlayerOwnInLineOne(player, 0);
+    let hasWonInLine = this.hasPlayerOwnInLine(player, 0, 0);
     return hasWonInLine;
   }
 
-  private hasPlayerOwnInLineOne(player: string, columnNumber: number): boolean {
-    if (this.moves.find(move => move.getPlayedRow() === 0 && move.getPlayedColumn() === columnNumber).getPlayer() === player) {
-      return columnNumber === this.columnNumber - 1 ? true : this.hasPlayerOwnInLineOne(player, ++columnNumber);
+  private hasPlayerOwnInLine(player: string, rowNumber: number, columnNumber: number): boolean {
+    let move = this.moves.find(move => move.getPlayedRow() === rowNumber && move.getPlayedColumn() === columnNumber);
+    if (move && move.getPlayer() === player) {
+      return columnNumber === this.columnNumber - 1 ? true : this.hasPlayerOwnInLine(player, rowNumber, ++columnNumber);
+    } else {
+      return rowNumber < this.rowNumber - 1 ? this.hasPlayerOwnInLine(player, ++rowNumber, 0) : false;
     }
-    return false;
   }
 }
