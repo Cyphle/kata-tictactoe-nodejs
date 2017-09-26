b30a794ad430ddc435ea43ab9601b6106f8cb763
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-26 20:07:41 +0200

    X wins in a row with board of 2x2

--- src/domain/TicTacToeGame.spec.ts
+++ src/domain/TicTacToeGame.spec.ts
@@ -15,4 +15,14 @@ describe('TicTacToGame.ts', () => {
 
     expect(game.getWinner()).to.equal('X');
   });
+
+  it('should have game won by X in row with board of 2x2', () => {
+    let game = new TicTacToeGame(2, 2);
+
+    game.play('X', { row: 0, column: 0 });
+    game.play('O', { row: 1, column: 0 });
+    game.play('X', { row: 0, column: 1 });
+
+    expect(game.getWinner()).to.equal('X');
+  });
 });
